cf02e21cae4bab0b09b7cb9ad78d6e8ba394b8c5
zakie <realzzaekkii@gmail.com>
2026-01-05 22:22:36 +0900

    feat: md 파일에서 프로모션 정보를 읽어 프로모션 객체 리스트를 반환한다.

--- src/main/java/store/domain/Promotion.java
+++ src/main/java/store/domain/Promotion.java
@@ -4,8 +4,8 @@ import java.util.Date;
 
 public record Promotion(
         String name,
-        String buy,
-        String get,
+        int buy,
+        int get,
         Date start_date,
         Date end_date
 ) {
--- src/main/java/store/view/FileInputView.java
+++ src/main/java/store/view/FileInputView.java
@@ -1,4 +1,39 @@
 package store.view;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import store.domain.Promotion;
+
 public class FileInputView {
+
+    public List<Promotion> readPromotions() throws IOException, ParseException {
+        List<String> lines = Files.readAllLines(Paths.get("src/main/resources/promotions.md"));
+        List<Promotion> promotions = new ArrayList<>();
+
+        boolean firstLine = true;
+        for (String line : lines) {
+            if (firstLine) {
+                firstLine = false;
+                continue;
+            }
+
+            String[] values = line.split(",");
+
+            String name = values[0];
+            int buy = Integer.parseInt(values[1]);
+            int get = Integer.parseInt(values[2]);
+            Date startDate = new SimpleDateFormat("yyyy-MM-dd").parse(values[3]);
+            Date endDate = new SimpleDateFormat("yyyy-MM-dd").parse(values[4]);
+
+            promotions.add(new Promotion(name, buy, get, startDate, endDate));
+        }
+
+        return promotions;
+    }
 }

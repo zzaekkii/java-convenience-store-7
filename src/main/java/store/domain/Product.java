a9b5202a2d0a3f70f5411c8a6f4ab881a0258461
zakie <realzzaekkii@gmail.com>
2026-01-05 22:32:09 +0900

    feat: md 파일에서 상품 정보를 읽어와 상품 리스트를 반환한다

--- src/main/java/store/domain/Product.java
+++ src/main/java/store/domain/Product.java
@@ -1,13 +1,15 @@
 package store.domain;
 
+import java.util.Optional;
+
 public class Product {
     private final String name;
     private final int price;
     private final int quantity;
-    private final Promotion promotion;
+    private final Optional<Promotion> promotion;
 
 
-    public Product(String name, int price, int quantity, Promotion promotion) {
+    public Product(String name, int price, int quantity, Optional<Promotion> promotion) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
--- src/main/java/store/view/FileInputView.java
+++ src/main/java/store/view/FileInputView.java
@@ -8,6 +8,8 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
+import store.domain.Product;
 import store.domain.Promotion;
 
 public class FileInputView {
@@ -35,5 +37,40 @@ public class FileInputView {
         }
 
         return promotions;
+    }
+
+    public List<Product> readProducts(List<Promotion> promotions) throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get("src/main/resources/products.md"));
+        List<Product> products = new ArrayList<>();
+
+        boolean firstLine = true;
+        for (String line : lines) {
+            if (firstLine) {
+                firstLine = false;
+                continue;
+            }
+
+            String[] values = line.split(",");
+
+            String name = values[0];
+            int price = Integer.parseInt(values[1]);
+            int quantity = Integer.parseInt(values[2]);
+            String promotionValue = values[3];
+
+            boolean existPromotion = false;
+            for (Promotion promotion : promotions) {
+                if (promotion.name().equals(promotionValue)) {
+                    products.add(new Product(name, price, quantity, Optional.of(promotion)));
+                    existPromotion = true;
+                    break;
+                }
+            }
+
+            if (!existPromotion) {
+                products.add(new Product(name, price, quantity, Optional.empty()));
+            }
+        }
+
+        return products;
     }
 }
